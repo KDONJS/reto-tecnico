81afc403c96e845f8445f40325c1206fdb408b0b
Devioz-csm <csm-dev@devioz.com>
2024-10-30 10:20:28 -0500

    update

--- infra/provider.tf
+++ infra/provider.tf
@@ -1,18 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-    awscc = {
-      source = "hashicorp/awscc"
-      version = "~> 1.16.1"
-    }
-  }
-}
-
-
 provider "aws" {
   region = "us-east-1"
-  version = "~> 4.0"
 }
